ca6fa89d8db14246401980f955c548e75b39a585
HayatoWada <hatoya.web.design@gmail.com>
2020-02-22 14:14:47 +0900

    modify environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDRLhr6CzG6KrIH0zc0SHqFPVVodWBao5Q',
+    authDomain: 'angular-template-a2ff7.firebaseapp.com',
+    databaseURL: 'https://angular-template-a2ff7.firebaseio.com',
+    projectId: 'angular-template-a2ff7',
+    storageBucket: 'angular-template-a2ff7.appspot.com',
+    messagingSenderId: '91476180592',
+    appId: '1:91476180592:web:e79e4d2eb18afb170f7eb5',
+    measurementId: 'G-0826YHHZ66'
+  }
 };
 
 /*
