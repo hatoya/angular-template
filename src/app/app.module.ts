c104fdc99b1bad6bcf1d6048743a59a279c27682
Hayato Wada <hatoyab@gmail.com>
2020-07-21 14:53:50 +0900

    modify form page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireStorageModule } from '@angular/fire/storage';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoadingComponent } from './component/atom/loading/loading.component';
 import { FormComponent } from './component/page/form/form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, IndexComponent, LoadingComponent, FormComponent],
@@ -26,7 +27,8 @@ import { FormComponent } from './component/page/form/form.component';
     AngularFireAuthModule,
     AngularFirestoreModule,
     AngularFireStorageModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [{ provide: NG_ENTITY_SERVICE_CONFIG, useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' } }],
   bootstrap: [AppComponent]
--- src/app/component/page/form/state/form.service.ts
+++ src/app/component/page/form/state/form.service.ts
@@ -1,9 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
 import { FormStore } from './form.store';
 
 @Injectable({ providedIn: 'root' })
 export class FormService {
-  constructor(private formStore: FormStore, private http: HttpClient) {}
+  constructor(private formStore: FormStore) {}
 }
