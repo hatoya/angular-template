a24a711c212e215b09e644005a75683013f709ee
Hayato Wada <hatoyab@gmail.com>
2020-09-02 12:57:43 +0900

    modify radio-group molecule component

--- src/app/component/molecule/radio-group/radio-group.component.ts
+++ src/app/component/molecule/radio-group/radio-group.component.ts
@@ -1,12 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, Self, ViewChildren } from '@angular/core';
+import { AbstractControl, ControlValueAccessor, NgControl } from '@angular/forms';
+import { IOption } from 'src/app/model/option.model';
 
 @Component({
   selector: 'app-radio-group',
   templateUrl: './radio-group.component.html',
   styleUrls: ['./radio-group.component.scss']
 })
-export class RadioGroupComponent implements OnInit {
-  constructor() {}
+export class RadioGroupComponent implements ControlValueAccessor, OnInit {
+  @Input() label = null;
+  @Input() options: IOption<any, any>[] = [];
 
-  ngOnInit(): void {}
+  @ViewChildren('radio') elements: ElementRef[] = [];
+
+  required = false;
+
+  onChange: (value: any) => void;
+  onTouched: (value: any) => void;
+
+  constructor(@Self() public control: NgControl) {
+    this.control.valueAccessor = this;
+  }
+
+  ngOnInit(): void {
+    const validator = this.control.control.validator;
+    this.required = validator ? validator({} as AbstractControl).required || false : false;
+  }
+
+  // ControlValueAccessor
+  writeValue(value: any) {
+    this.elements.map(element => element.nativeElement).forEach(element => (element.checked = element.value === value));
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
 }
