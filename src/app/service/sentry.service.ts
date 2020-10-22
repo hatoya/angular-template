c6d9e7f04a15012f54ea06ae0071f8fa527f1a00
Hayato Wada <hatoyab@gmail.com>
2020-10-23 00:02:10 +0900

    modify sentry service

--- src/app/service/sentry.service.ts
+++ src/app/service/sentry.service.ts
@@ -1,23 +1,6 @@
-import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Injectable } from '@angular/core';
 import * as Sentry from '@sentry/angular';
-import { Integrations } from '@sentry/tracing';
-import { environment } from 'src/environments/environment';
 import { IAccount } from '../model/account.model';
-
-Sentry.init({
-  dsn: 'https://55f8148dd8d94a24bb560ca39312c9c4@o458742.ingest.sentry.io/5477902',
-  environment: environment.production ? 'Production' : 'Development',
-  integrations: [
-    new Integrations.BrowserTracing({
-      tracingOrigins: ['localhost', `https://${environment.firebase.projectId}.web.app`],
-      routingInstrumentation: Sentry.routingInstrumentation
-    })
-  ],
-  maxBreadcrumbs: 50,
-  tracesSampleRate: 1.0,
-  logLevel: 3
-});
 
 @Injectable({
   providedIn: 'root'
@@ -26,12 +9,7 @@ export class SentryService implements ErrorHandler {
   constructor() {}
 
   handleError(error: any) {
-    const eventId = Sentry.captureException(error.originalError || error);
-    if (Error instanceof HttpErrorResponse) {
-      console.log(error.status);
-    } else {
-      Sentry.showReportDialog({ eventId });
-    }
+    Sentry.captureException(error.originalError || error);
   }
 
   setUser(account: IAccount) {
