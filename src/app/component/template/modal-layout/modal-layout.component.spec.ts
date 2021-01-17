1ad9d3b9de74b30db4fb138f4c4eac2196a53c34
Hayato Wada <hatoyab@gmail.com>
2021-01-18 06:28:10 +0900

    update to angular11

--- src/app/component/template/modal-layout/modal-layout.component.spec.ts
+++ src/app/component/template/modal-layout/modal-layout.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ModalLayoutComponent } from './modal-layout.component';
 
@@ -6,7 +6,7 @@ describe('ModalLayoutComponent', () => {
   let component: ModalLayoutComponent;
   let fixture: ComponentFixture<ModalLayoutComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ModalLayoutComponent]
     }).compileComponents();
