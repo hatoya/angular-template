c21312ff15a2a1e04e3d245e8a42c1333adeb16d
Hayato Wada <hatoyab@gmail.com>
2021-04-20 10:15:36 +0900

    modify textarea atom component

--- src/app/component/atom/textarea/textarea.component.ts
+++ src/app/component/atom/textarea/textarea.component.ts
@@ -1,5 +1,7 @@
-import { Component, ElementRef, Input, OnInit, Self, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, Optional, Self, ViewChild } from '@angular/core';
 import { AbstractControl, ControlValueAccessor, NgControl } from '@angular/forms';
+import { EFormStatus } from 'src/app/enum/form-status.enum';
+import { ValidationMessageService } from 'src/app/service/validation-message.service';
 
 @Component({
   selector: 'app-textarea',
@@ -10,6 +12,7 @@ export class TextareaComponent implements ControlValueAccessor, OnInit {
   @Input() type = 'default';
   @Input() label = null;
   @Input() placeholder = '';
+  @Input() status = EFormStatus.EDITABLE;
 
   @ViewChild('textarea') element: ElementRef;
 
@@ -18,19 +21,34 @@ export class TextareaComponent implements ControlValueAccessor, OnInit {
   onChange: (value: any) => void;
   onTouched: (value: any) => void;
 
-  constructor(@Self() public control: NgControl) {
-    this.control.valueAccessor = this;
+  constructor(
+    @Self() @Optional() public control: NgControl,
+    public validationMessageService: ValidationMessageService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {
+    if (this.control) {
+      this.control.valueAccessor = this;
+    }
   }
 
   ngOnInit(): void {
-    const validator = this.control.control.validator;
+    const validator = this.control?.control.validator;
     this.required = validator ? validator({} as AbstractControl)?.required || false : false;
+  }
+
+  get isReadOnly() {
+    return this.status === EFormStatus.READONLY;
+  }
+
+  get isDisabled() {
+    return this.status === EFormStatus.DISABLED;
   }
 
   // ControlValueAccessor
   writeValue(value: any) {
     if (this.element) {
       this.element.nativeElement.value = value;
+      this.changeDetectorRef.detectChanges();
     }
   }
 
