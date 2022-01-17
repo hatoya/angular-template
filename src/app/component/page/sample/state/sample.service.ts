b749c08f37cbf45f09d561e261a9f9b622fa9828
hatoya <hatoyab@gmail.com>
2022-01-17 12:24:47 +0900

    Update sample.service.ts

--- src/app/component/page/sample/state/sample.service.ts
+++ src/app/component/page/sample/state/sample.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators';
 import { SampleStore } from './sample.store';
 
 @Injectable({ providedIn: 'root' })
