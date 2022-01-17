3858e3810a055b6974f65751e3b6c0f8b75a540c
hatoya <hatoyab@gmail.com>
2022-01-17 09:26:04 +0900

    Create account-role.enum.ts

---
+++ src/app/enum/account-role.enum.ts
@@ -0,0 +1,11 @@
+export enum EAccountRole {
+  ADMIN = 'admin',
+  OWNER = 'owner',
+  MEMBER = 'member'
+}
+
+export enum EAccountRoleLabel {
+  admin = '管理者',
+  owner = 'オーナー',
+  member = 'メンバー'
+}
