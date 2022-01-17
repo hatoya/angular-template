fa5470b48850083c10cfe70b053d2d17b8deee7b
hatoya <hatoyab@gmail.com>
2022-01-17 09:26:19 +0900

    Update option.model.ts

--- src/app/model/option.model.ts
+++ src/app/model/option.model.ts
@@ -1,5 +1,11 @@
-export interface IOption<U, T> {
-  value: U;
-  label: T;
-  disabled?: boolean;
+export interface IOption<U = string, T = string> {
+  value: U | null;
+  label: T | null;
+}
+
+export function createOption<U = string, T = string>(value: U, label: T): IOption<U, T> {
+  return {
+    value: value || null,
+    label: label || null
+  };
 }
