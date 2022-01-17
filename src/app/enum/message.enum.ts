b761ad64d9649831e8ccfe9fc24a68d2cde50271
hatoya <hatoyab@gmail.com>
2022-01-17 09:15:53 +0900

    Create message.enum.ts

---
+++ src/app/enum/message.enum.ts
@@ -0,0 +1,7 @@
+export enum EMessage {
+  SUCCESS = 'SUCCESS',
+  NOT_FOUND = 'NOT FOUND',
+  VALIDATION_ERROR = 'バリデーションエラー',
+  ADVANCE_GUARD_ERROR = '初期ロードでエラーが発生しました\nリロードしてください',
+  BROKEN = 'エラーが発生しました'
+}
