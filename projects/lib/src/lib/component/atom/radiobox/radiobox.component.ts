228786f05b3ef528858a1ce6ad66493fe1ae1b1c
hatoya <hatoyab@gmail.com>
2022-01-22 09:50:24 +0900

    update radiobox component

--- projects/lib/src/lib/component/atom/radiobox/radiobox.component.ts
+++ projects/lib/src/lib/component/atom/radiobox/radiobox.component.ts
@@ -2,6 +2,8 @@ import { Component, ElementRef, Input, OnInit, Optional, Self, ViewChildren } fr
 import { NgControl } from '@angular/forms';
 import { faCircle, faDotCircle } from '@fortawesome/pro-regular-svg-icons';
 import { AbstractControl, ControlValueAccessor } from '@ngneat/reactive-forms';
+import { EFormLayout } from 'src/app/enum/form-layout.enum';
+import { EFormStatus } from 'src/app/enum/form-status.enum';
 import { IOption } from 'src/app/model/option.model';
 import { ValidationMessageService } from 'src/app/service/validation-message.service';
 
@@ -15,10 +17,11 @@ export class RadioboxComponent implements ControlValueAccessor<string | number>,
   @Input() name = '';
   @Input() label = null;
   @Input() options: IOption<any, any>[] = [];
+  @Input() status = EFormStatus.EDITABLE;
+  @Input() layout = EFormLayout.DEFAULT;
 
   @ViewChildren('radiobox') elements: ElementRef[] = [];
 
-  required = false;
   faCircle = faCircle;
   faDotCircle = faDotCircle;
 
@@ -31,9 +34,23 @@ export class RadioboxComponent implements ControlValueAccessor<string | number>,
     }
   }
 
-  ngOnInit(): void {
-    const validator = this.control?.control.validator;
-    this.required = validator ? validator({} as AbstractControl<string | number>)?.required || false : false;
+  ngOnInit(): void {}
+
+  get required() {
+    const validator = this.control?.control?.validator;
+    return validator ? validator({} as AbstractControl<string | number>)?.required || false : false;
+  }
+
+  get isReadOnly() {
+    return this.status === EFormStatus.READONLY;
+  }
+
+  get isDisabled() {
+    return this.status === EFormStatus.DISABLED;
+  }
+
+  get isSideLayout() {
+    return this.layout === EFormLayout.SIDE;
   }
 
   // ControlValueAccessor
