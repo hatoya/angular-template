9a38145cbddaae97629d7d6ea64283b680cf1f4b
hatoya <hatoyab@gmail.com>
2022-01-25 12:32:02 +0900

    Update validation.enum.ts

--- src/app/enum/validation.enum.ts
+++ src/app/enum/validation.enum.ts
@@ -2,5 +2,7 @@ export enum EValidation {
   required = 'This field is required',
   email = 'Email format is incorrect',
   minlength = 'Expect ${requiredLength} but got ${actualLength}',
-  maxlength = 'Expect ${requiredLength} but got ${actualLength}'
+  maxlength = 'Expect ${requiredLength} but got ${actualLength}',
+  included = 'Invalid value',
+  daterange = 'Invalid value'
 }
