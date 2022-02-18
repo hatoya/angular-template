c9553fc651f30787e113526e9ebc36460890705a
hatoya <hatoyab@gmail.com>
2022-02-18 16:41:14 +0900

    update sample components

--- src/app/component/page/sample/state/sample.service.spec.ts
+++ src/app/component/page/sample/state/sample.service.spec.ts
@@ -2,11 +2,11 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder } from '@ngneat/reactive-forms';
-import { InputRangeService } from '../../../../component/molecule/input-range/state/input-range.service';
+import { InputRangeService } from 'src/app/component/molecule/input-range/state/input-range.service';
 import { SampleService } from './sample.service';
 import { SampleStore } from './sample.store';
 
-jest.mock('../../../../component/molecule/input-range/state/input-range.service');
+jest.mock('src/app/component/molecule/input-range/state/input-range.service');
 
 describe('SampleService', () => {
   let sampleService: SampleService;
