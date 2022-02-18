bc7df099188bc6428e0fdca8892bfdaa11b92bef
hatoya <hatoyab@gmail.com>
2022-02-18 16:33:53 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,14 @@ require('jest-preset-angular/ngcc-jest-processor');
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)']
+  transformIgnorePatterns: ['node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
 };
