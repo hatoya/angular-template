e46ba2f1a6ba8b2031e68a2cfe12862980653b76
hatoya <hatoyab@gmail.com>
2022-02-27 10:16:11 +0900

    update sample component

--- src/app/component/page/sample/sample.component.ts
+++ src/app/component/page/sample/sample.component.ts
@@ -1,8 +1,10 @@
 import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { ControlsOf, FormGroup } from '@ngneat/reactive-forms';
+import { EButtonType } from 'src/app/enum/button-type.enum';
 import { EFormLayout } from 'src/app/enum/form-layout.enum';
 import { EFormStatus } from 'src/app/enum/form-status.enum';
 import { EInputType } from 'src/app/enum/input-type.enum';
+import { ModalSampleService } from '../../modal/modal-sample/state/modal-sample.service';
 import { SnackbarService } from '../../snackbar/state/snackbar.service';
 import { SampleQuery } from './state/sample.query';
 import { ISample, SampleService } from './state/sample.service';
@@ -18,12 +20,14 @@ export class SampleComponent implements OnInit, AfterViewInit, OnDestroy {
   formLayoutEnum = EFormLayout;
   inputTypeEnum = EInputType;
   formStatusEnum = EFormStatus;
+  buttonTypeENum = EButtonType;
 
   constructor(
     public query: SampleQuery,
     private service: SampleService,
     private store: SampleStore,
-    private snackbarService: SnackbarService
+    private snackbarService: SnackbarService,
+    public modalSampleService: ModalSampleService
   ) {
     this.createFormGroup();
   }
