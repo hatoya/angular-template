c73f9366a14b28355714a850984fe6083f4c2651
hatoya <hatoyab@gmail.com>
2022-02-27 10:16:05 +0900

    update modal-sample component

--- src/app/component/modal/modal-sample/state/modal-sample.service.ts
+++ src/app/component/modal/modal-sample/state/modal-sample.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ModalService } from '../../state/modal.service';
+import { ModalSampleComponent } from '../modal-sample.component';
 import { ModalSampleStore } from './modal-sample.store';
 
 @Injectable({ providedIn: 'root' })
 export class ModalSampleService {
-  constructor(private modalSampleStore: ModalSampleStore, private http: HttpClient) {}
+  constructor(private store: ModalSampleStore, private modalService: ModalService) {}
+
+  open() {
+    this.modalService.open(ModalSampleComponent);
+  }
 }
