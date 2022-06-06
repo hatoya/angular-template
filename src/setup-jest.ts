949109cf927d7185f22b11df8ddaa19d15fd155f
hatoya <hatoyab@gmail.com>
2022-06-07 04:30:52 +0900

    Update setup-jest.ts

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,1 +1,8 @@
 import 'jest-preset-angular/setup-jest';
+import { randomBytes } from 'crypto';
+
+Object.defineProperty(globalThis, 'crypto', {
+  value: {
+    getRandomValues: arr => randomBytes(arr.length)
+  }
+});
