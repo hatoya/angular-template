5a19a276fa752abc468b0760fb92b122a957744a
hatoya <hatoyab@gmail.com>
2022-06-09 05:51:12 +0900

    Revert "Update setup-jest.ts"
    This reverts commit 949109cf927d7185f22b11df8ddaa19d15fd155f.

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,8 +1,1 @@
 import 'jest-preset-angular/setup-jest';
-import { randomBytes } from 'crypto';
-
-Object.defineProperty(globalThis, 'crypto', {
-  value: {
-    getRandomValues: arr => randomBytes(arr.length)
-  }
-});
