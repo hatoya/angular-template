c55867b5a141758fb4f7189ae3553fc92c401328
hatoya <hatoyab@gmail.com>
2022-10-20 06:56:34 +0900

    Create .stylelintrc.js

---
+++ .stylelintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: ['stylelint-prettier', 'stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order', 'stylelint-prettier/recommended', 'stylelint-config-prettier'],
+  ignoreFiles: ['**/node_modules/**'],
+  customSyntax: 'postcss-scss',
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'no-empty-source': null,
+    'import-notation': 'string',
+    'selector-class-pattern': null,
+    'block-no-empty': null,
+    'no-descending-specificity': null,
+    'selector-pseudo-element-no-unknown': [true, { ignorePseudoElements: ['/^ng-/'] }]
+  }
+};
