f9dd1a9e243bed88a53223a754e0207be1da6b55
hatoya <hatoyab@gmail.com>
2023-08-11 19:11:36 +0900

    update range component

--- src/app/component/atom/range/range.service.spec.ts
+++ src/app/component/atom/range/range.service.spec.ts
@@ -1,20 +1,17 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RangeService } from './range.service';
-import { RangeStore } from './range.store';
 
 describe('RangeService', () => {
   let rangeService: RangeService;
-  let rangeStore: RangeStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [RangeService, RangeStore],
+      providers: [RangeService],
       imports: [HttpClientTestingModule]
     });
 
     rangeService = TestBed.inject(RangeService);
-    rangeStore = TestBed.inject(RangeStore);
   });
 
   it('should be created', () => {
