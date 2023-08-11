79fcddae563a80c110fbee9ad8dd5ae7e27261a1
hatoya <hatoyab@gmail.com>
2023-08-11 19:14:09 +0900

    update input-range component

--- src/app/component/molecule/input-range/input-range.service.spec.ts
+++ src/app/component/molecule/input-range/input-range.service.spec.ts
@@ -2,20 +2,17 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { Validators } from '@angular/forms';
 import { InputRangeService } from './input-range.service';
-import { InputRangeStore } from './input-range.store';
 
 describe('InputRangeService', () => {
   let inputRangeService: InputRangeService;
-  let inputRangeStore: InputRangeStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [InputRangeService, InputRangeStore],
+      providers: [InputRangeService],
       imports: [HttpClientTestingModule]
     });
 
     inputRangeService = TestBed.inject(InputRangeService);
-    inputRangeStore = TestBed.inject(InputRangeStore);
   });
 
   it('should be created', () => {
--- src/app/component/molecule/input-range/input-range.service.ts
+++ src/app/component/molecule/input-range/input-range.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, ValidatorFn } from '@angular/forms';
-import { InputRangeStore } from './input-range.store';
 
 @Injectable({ providedIn: 'root' })
 export class InputRangeService {
-  constructor(private store: InputRangeStore) {}
+  constructor() {}
 
   createFormGroup(fromValidators: ValidatorFn[] = [], toValidators: ValidatorFn[] = [], formGroupValidators: ValidatorFn[] = []) {
     return new FormGroup(
