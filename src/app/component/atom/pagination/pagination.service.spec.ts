7a4796d3999dee3c087e789bbcf966e5dc5ac7e5
hatoya <hatoyab@gmail.com>
2023-08-11 19:13:18 +0900

    update pagination component

--- src/app/component/atom/pagination/pagination.service.spec.ts
+++ src/app/component/atom/pagination/pagination.service.spec.ts
@@ -1,20 +1,17 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { PaginationService } from './pagination.service';
-import { PaginationStore } from './pagination.store';
 
 describe('PaginationService', () => {
   let paginationService: PaginationService;
-  let paginationStore: PaginationStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PaginationService, PaginationStore],
+      providers: [PaginationService],
       imports: [HttpClientTestingModule]
     });
 
     paginationService = TestBed.inject(PaginationService);
-    paginationStore = TestBed.inject(PaginationStore);
   });
 
   it('should be created', () => {
--- src/app/component/atom/pagination/state/pagination.service.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { PaginationStore } from './pagination.store';
-
-@Injectable({ providedIn: 'root' })
-export class PaginationService {
-  constructor(private paginationStore: PaginationStore, private http: HttpClient) {}
-}
