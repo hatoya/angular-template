cc1be3277ee2e629d5cc877a1c420406e5368065
hatoya <hatoyab@gmail.com>
2023-08-11 19:12:28 +0900

    update datalist component

--- src/app/component/atom/datalist/datalist.service.spec.ts
+++ src/app/component/atom/datalist/datalist.service.spec.ts
@@ -1,20 +1,17 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { DatalistService } from './datalist.service';
-import { DatalistStore } from './datalist.store';
 
 describe('DatalistService', () => {
   let datalistService: DatalistService;
-  let datalistStore: DatalistStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DatalistService, DatalistStore],
+      providers: [DatalistService],
       imports: [HttpClientTestingModule]
     });
 
     datalistService = TestBed.inject(DatalistService);
-    datalistStore = TestBed.inject(DatalistStore);
   });
 
   it('should be created', () => {
--- src/app/component/atom/datalist/state/datalist.service.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { DatalistStore } from './datalist.store';
-
-@Injectable({ providedIn: 'root' })
-export class DatalistService {
-  constructor(private datalistStore: DatalistStore, private http: HttpClient) {}
-}
