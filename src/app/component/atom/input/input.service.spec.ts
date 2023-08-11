c5daec33be001f50412765f0cb1413dd5e0952ba
hatoya <hatoyab@gmail.com>
2023-08-11 19:10:50 +0900

    update input component

--- src/app/component/atom/input/input.service.spec.ts
+++ src/app/component/atom/input/input.service.spec.ts
@@ -1,20 +1,17 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { InputService } from './input.service';
-import { InputStore } from './input.store';
 
 describe('InputService', () => {
   let inputService: InputService;
-  let inputStore: InputStore;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [InputService, InputStore],
+      providers: [InputService],
       imports: [HttpClientTestingModule]
     });
 
     inputService = TestBed.inject(InputService);
-    inputStore = TestBed.inject(InputStore);
   });
 
   it('should be created', () => {
